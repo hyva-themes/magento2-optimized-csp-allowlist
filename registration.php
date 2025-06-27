b9b46dd0c029f60d06d1deb14f9a9cd2bf30851c
Jeroen Boersma <jeroen@srcode.nl>
2025-06-27 15:47:41 +0200

    Corrected the name of module to match the namespace

--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Hyva_OptimizedAllowlist', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Hyva_OptimizedCspAllowlist', __DIR__);
