3ad3403a4cd669a59aba12674411860abc0d2f69
Jeroen Boersma <jeroen@srcode.nl>
2025-06-27 15:49:13 +0200

    Added new configuration option disable all csp_whitelist.xml

--- Model/Config.php
+++ Model/Config.php
@@ -8,7 +8,9 @@ use Magento\Store\Model\ScopeInterface;
 class Config
 {
 
-    public const string XMLPATH_CSP_ALLOWLIST_OPTIMIZATION_ENABLED = 'csp/allowlist/optimization_enabled';
+    public const XMLPATH_CSP_ALLOWLIST_OPTIMIZATION_ENABLED = 'csp/allowlist/optimization_enabled';
+
+    public const XMLPATH_CSP_ALLOWLIST_MODULES_DISABLED = 'csp/allowlist/modules_disabled';
 
     public function __construct(
         private ScopeConfigInterface $scopeConfig
@@ -26,4 +28,15 @@ class Config
         );
     }
 
+    public function isAllowlistModulesDisabled(
+        $scope = ScopeInterface::SCOPE_STORE,
+        $scopeCode = null
+    ): bool
+    {
+        return (bool)$this->scopeConfig->getValue(
+            self::XMLPATH_CSP_ALLOWLIST_MODULES_DISABLED,
+            $scope,
+            $scopeCode
+        );
+    }
 }
